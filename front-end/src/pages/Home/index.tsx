b42773ca1c0e457d5e59b3f53c75b583273539a6
rajahwu <vmradford@gmail.com>
2023-10-25 17:40:20 -0400

    add user

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 
 export default function HomePage() {
     const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setComfirmPassword] = useState("")
     return (
         <div>
             <div>HomePage</div>
             <form action="" onSubmit={(e) => {
                 e.preventDefault()
+                if(password !== confirmPassword) throw new Error('Passwords do not match')
                 console.log({ username })
             }}>
 
-                <label htmlFor="user"></label>
                 <input
                     id="userInput"
                     className="form-input border-black"
@@ -18,6 +20,25 @@ export default function HomePage() {
                     name="user"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
+                    placeholder='username'
+                />
+                <input
+                    id="password"
+                    className="form-input border-black"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder='password'
+                />
+                <input
+                    id="confirmPassword"
+                    className="form-input border-black"
+                    type="password"
+                    name="password"
+                    value={confirmPassword}
+                    onChange={(e) => setComfirmPassword(e.target.value)}
+                    placeholder='confirm password'
                 />
                 <input type="submit" name="submit" id="" />
             </form>
