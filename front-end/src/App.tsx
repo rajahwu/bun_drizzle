e1e9b0b05754b77aae0b372b07f547c44bbd8975
rajahwu <vmradford@gmail.com>
2023-10-25 16:36:15 -0400

    add react router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="text-3xl font-bold uppercase text-center mt-5">Project</div>
+    <div className="title">Project</div>
   )
 }
 
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route
+  path="/"
+  element={<App />}
+  />
+))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
